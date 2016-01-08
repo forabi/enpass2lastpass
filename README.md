26a1baf562804f437ea2add493a18cdb9a8e842e
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-01-08 02:56:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Enpass2LastPass
+=================
+
+A little script that converts plain text files produced by Enpass export tool to LastPass importable CSV.
+
+Usage
+------
+
+1. Install globally:
+
+  ```shell
+  npm install -g enpass2lastpass
+  ```
+  
+2. The script accepts a path to a text file and outputs to stdout:
+
+  ```shell
+  enpass2lastpass ./enpass-export.txt > lastpass-import.csv
+  ```
+  
+3. Done!
+
+
+Contribution
+------------
+Bug reports and pull requests are welcome!
---
+++ index.js
@@ -0,0 +1,68 @@
+#!/bin/env node
+'use strict';
+
+const fs = require('fs');
+const url = require('url');
+const _ = require('highland');
+const args = process.argv.slice(2);
+
+const filename = args[0];
+
+if (!filename) {
+    process.stderr.write('You must specify an Enpass export file to process.\n');
+    process.exit(1);
+    return;
+}
+
+let a = 0;
+
+process.stdout.write('name,url,username,password,extra\n')
+
+_(fs.createReadStream(filename, 'utf8'))
+.splitBy(/\n\n/igm)
+.map(s => s.trim().split('\n'))
+.map(s => {
+    return s.slice(1).map(l => {
+        const m = l.match(
+            /^([a-z\s]+)\s(.+)$/i);
+        if (m) {
+            return [m[1].toLowerCase(), m[2]];
+        } else {
+            return ['notes', l];
+        }
+    })
+    .filter(x => x)
+    .concat([['name', s[0]]])
+    .reduce((o, field) => {
+        o[field[0]] = field[1]
+        return o
+    }, {})
+})
+.filter(x => x.password && x.url)
+.map(x => {
+    if (!x.url) return x;
+    const oldUrl = url.parse(x.url);
+    
+    x.url = url.format({
+        host: oldUrl.host,
+        protocol: oldUrl.protocol,
+        auth: oldUrl.auth
+    });
+    
+    return x;
+})
+.map(x => {
+    if (!x.name) return x;
+    const m = x.name.match(/^(Generated Password for|إنشاء كلمة السر لـ )\s?(.+)$/i)
+    if (m) {
+        x.name = m[2];
+    }
+    return x;
+})
+.map(x => {
+    x.username = x.username || x.email || 'mfawwaz93@gmail.com';
+    return x;
+})
+.map(x => [x.name, x.url, x.username, x.password, x.notes].join() + '\n')
+.tap(x => a++)
+.pipe(process.stdout)
