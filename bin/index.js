68cc4095cd6c4eea75f499baaba25513adfd4d7d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-01-11 23:32:08 +0200

    Extract functions in index.js for testing

--- bin/index.js
+++ bin/index.js
@@ -5,15 +5,46 @@ const fs = require('fs');
 const _ = require('highland');
 const toCSV = require('../lib/convert');
 
-const args = process.argv.slice(2);
-
-const filename = args[0];
-
-if (!filename) {
-  process.stderr.write('You must specify an Enpass export file to process.\n');
+const fatal = e => {
+  process.stderr.write(e.message + '\n');
   process.exit(1);
+};
+
+const parseArgs = args => require('minimist')(args, {
+  string: 'default-email',
+  boolean: ['clean-names', 'clean-urls', 'remove-unuseful'],
+  alias: {
+    'clean-names': 'n',
+    'clean-urls': 'l',
+    'remove-unuseful': ['i', 'ignore-empty'],
+    output: 'o',
+  },
+  default: {
+    'default-email': null,
+    'clean-names': true,
+    'clean-urls': true,
+    'remove-unuseful': false,
+    output: undefined,
+  },
+  unknown: (a) => {
+    if (a === args[0]) return true;
+    fatal(`Unknown argument ${a}.\n`);
+  },
+});
+
+const fileToCSVStream = (filename, options) => {
+  return _(fs.createReadStream(filename, 'utf8'))
+  .through(toCSV(options))
+  .on('error', fatal);
+};
+
+const _args = parseArgs(process.argv.slice(2));
+
+if (!_args._[0]) {
+  fatal('You must specify an Enpass export file to process.\n');
 }
 
-_(fs.createReadStream(filename, 'utf8'))
-.through(toCSV)
+fileToCSVStream(_args._[0], _args)
 .pipe(process.stdout);
+
+module.exports = { parseArgs, fileToCSVStream, fatal };
